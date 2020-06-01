a0fe83953fa48e9ffffa597db6761ac6f41d9029
priyanka-roychoudhury <46439349+priyanka-roychoudhury@users.noreply.github.com>
2020-06-01 23:36:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -39,7 +39,7 @@ function setup(){
     bird = new Bird(100,100);
 
     //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:100});
+    slingShot = new SlingShot(bird.body,{x:200, y:100});
 }
 
 function draw(){
